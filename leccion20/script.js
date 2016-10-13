53e4fec8248c7f0a9868e2865a1e8eb79eddb134
susan-TG <susana.tralma.g@gmail.com>
2016-10-13 09:30:09 -0300

    parte dos del ejercicio

--- leccion20/script.js
+++ leccion20/script.js
@@ -1,6 +1,15 @@
 // tu codigo va aca
 function callbackPromedio (a, b, c, callback) {
-			var suma = ( a+b+c );
-			var promedio = (suma / 3);
-			return callback(promedio);
-		}
+	var suma = ( a+b+c );
+	var promedio = (suma / 3);
+	return callback(promedio);
+}
+
+function segundero(){
+	var segundos = 0;
+	
+	setInterval (function() {
+ 		segundos = segundos +1;
+ 		document.getElementById("titulo").innerHTML = segundos;
+	}, 1000);
+} 
